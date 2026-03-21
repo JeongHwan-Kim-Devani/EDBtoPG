8093bff91ad8baeae05182729528ad5efa7b3fa4
Kei-JH <95085408+Kei-JH@users.noreply.github.com>
2026-03-22 04:13:01 +0900

    Exclude htf/htp/xmltype builtins from precheck results

--- EPAS_PRECHECK/migration_report_queries.sql
+++ EPAS_PRECHECK/migration_report_queries.sql
@@ -104,21 +104,7 @@ FROM (
     WHERE n.nspname NOT IN ('pg_catalog', 'information_schema', 'sys', 'dbo', 'sys_catalog', 'enterprisedb')
       AND n.nspname NOT LIKE 'dbms_%'
       AND n.nspname NOT LIKE 'utl_%'
-      AND NOT (
-        lower(p.proname) ~ '^(htf|htp|xmltype)'
-        AND (
-          p.prosrc LIKE '$__EDBwrapped__$$PROTOCOL2$%'
-          OR lower(pg_catalog.pg_get_userbyid(p.proowner)) = 'enterprisedb'
-          OR EXISTS (
-            SELECT 1
-            FROM pg_depend d2
-            JOIN pg_extension e2 ON e2.oid = d2.refobjid
-            WHERE d2.classid = 'pg_proc'::regclass
-              AND d2.objid = p.oid
-              AND d2.deptype = 'e'
-          )
-        )
-      )
+      AND lower(p.proname) !~ '^(htf|htp|xmltype)'
       AND NOT EXISTS (
         SELECT 1
         FROM pg_depend d
@@ -182,21 +168,7 @@ FROM (
     JOIN pg_namespace n ON p.pronamespace = n.oid,
     LATERAL regexp_matches(lower(p.prosrc), '(\m(?:dbms_crypto(?:\.[a-z0-9_]+)?|clob|bfile|raw|greatest|least|sysdate|systimestamp|rownum|rowid|level|nvl|nvl2|decode|add_months|months_between|last_day|next_day|instr|listagg|wm_concat|dual|minus|sys_connect_by_path|connect_by_root|connect_by_isleaf|pragma|sqlcode|sqlerrm|raise_application_error|numtodsinterval|numtoyminterval|sys_extract_utc|tz_offset|dbtimezone|sessiontimezone|lnnvl|nanvl|ratio_to_report|substrb|instrb|lengthb)\M)', 'g') AS m
     WHERE n.nspname NOT IN ('pg_catalog', 'information_schema', 'sys', 'dbo', 'sys_catalog', 'enterprisedb')
-      AND NOT (
-        lower(p.proname) ~ '^(htf|htp|xmltype)'
-        AND (
-          p.prosrc LIKE '$__EDBwrapped__$$PROTOCOL2$%'
-          OR lower(pg_catalog.pg_get_userbyid(p.proowner)) = 'enterprisedb'
-          OR EXISTS (
-            SELECT 1
-            FROM pg_depend d2
-            JOIN pg_extension e2 ON e2.oid = d2.refobjid
-            WHERE d2.classid = 'pg_proc'::regclass
-              AND d2.objid = p.oid
-              AND d2.deptype = 'e'
-          )
-        )
-      )
+      AND lower(p.proname) !~ '^(htf|htp|xmltype)'
       AND NOT EXISTS (
         SELECT 1
         FROM pg_depend d
@@ -217,21 +189,7 @@ FROM (
       AND n.nspname NOT LIKE 'dbms_%'
       AND n.nspname NOT LIKE 'utl_%'
       AND p.proname !~* '^((dbms|utl|owa|htp|htf)(_|\.)|aq\$)'
-      AND NOT (
-        lower(p.proname) ~ '^(htf|htp|xmltype)'
-        AND (
-          p.prosrc LIKE '$__EDBwrapped__$$PROTOCOL2$%'
-          OR lower(pg_catalog.pg_get_userbyid(p.proowner)) = 'enterprisedb'
-          OR EXISTS (
-            SELECT 1
-            FROM pg_depend d2
-            JOIN pg_extension e2 ON e2.oid = d2.refobjid
-            WHERE d2.classid = 'pg_proc'::regclass
-              AND d2.objid = p.oid
-              AND d2.deptype = 'e'
-          )
-        )
-      )
+      AND lower(p.proname) !~ '^(htf|htp|xmltype)'
       AND NOT EXISTS (
         SELECT 1
         FROM pg_depend d
@@ -485,21 +443,7 @@ FROM pg_proc p
 JOIN pg_namespace n ON p.pronamespace = n.oid
 LEFT JOIN pg_catalog.pg_language l ON l.oid = p.prolang
 WHERE n.nspname NOT IN ('pg_catalog', 'information_schema', 'sys', 'dbo', 'sys_catalog', 'enterprisedb')
-  AND NOT (
-    lower(p.proname) ~ '^(htf|htp|xmltype)'
-    AND (
-      p.prosrc LIKE '$__EDBwrapped__$$PROTOCOL2$%'
-      OR lower(pg_catalog.pg_get_userbyid(p.proowner)) = 'enterprisedb'
-      OR EXISTS (
-        SELECT 1
-        FROM pg_depend d2
-        JOIN pg_extension e2 ON e2.oid = d2.refobjid
-        WHERE d2.classid = 'pg_proc'::regclass
-          AND d2.objid = p.oid
-          AND d2.deptype = 'e'
-      )
-    )
-  )
+  AND lower(p.proname) !~ '^(htf|htp|xmltype)'
 UNION ALL
 SELECT 'V' AS object_type,
        v.schemaname AS schema_name,
@@ -535,21 +479,7 @@ FROM pg_proc p
 JOIN pg_namespace n ON p.pronamespace = n.oid
 LEFT JOIN pg_catalog.pg_language l ON l.oid = p.prolang
 WHERE n.nspname NOT IN ('pg_catalog', 'information_schema', 'sys', 'dbo', 'sys_catalog', 'enterprisedb')
-  AND NOT (
-    lower(p.proname) ~ '^(htf|htp|xmltype)'
-    AND (
-      p.prosrc LIKE '$__EDBwrapped__$$PROTOCOL2$%'
-      OR lower(pg_catalog.pg_get_userbyid(p.proowner)) = 'enterprisedb'
-      OR EXISTS (
-        SELECT 1
-        FROM pg_depend d2
-        JOIN pg_extension e2 ON e2.oid = d2.refobjid
-        WHERE d2.classid = 'pg_proc'::regclass
-          AND d2.objid = p.oid
-          AND d2.deptype = 'e'
-      )
-    )
-  )
+  AND lower(p.proname) !~ '^(htf|htp|xmltype)'
 UNION ALL
 SELECT 'V' AS object_type,
        v.schemaname AS schema_name,
